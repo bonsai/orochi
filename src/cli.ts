19f52737534b2561d2eb487213ca25dbd7e51727
bonsai <onsen.bonsai@gmail.com>
2026-09-19 23:06:43 +0900

    refactor: remove ambiguous src layout

--- src/cli.ts
+++
@@ -1,9 +0,0 @@
-import { resolveProject } from "./core/project.ts";
-
-const [command, value] = Deno.args;
-
-if (command === "resolve" && value) {
-  console.log(JSON.stringify(resolveProject(value), null, 2));
-} else {
-  console.log("orochi <resolve|open|group>");
-}
